078f90001a793b9c468f9c5ffcc64b3a11e4864e
RubenBrocke <ruben@marienhove.nl>
2017-11-28 22:56:44 +0100

    Initial commit

---
+++ src/Server.cr
@@ -0,0 +1,37 @@
+require "kemal"
+require "db"
+require "sqlite3"
+
+db = DB.open "sqlite3://./data.db" 
+#Create if not yet created
+db.exec("DROP TABLE times")
+db.exec("CREATE TABLE times (id string)")
+puts "CREATED NEW DATABASE"
+
+begin 
+#Showing basic information about the system
+get "/" do
+  db.query("SELECT * FROM times") do |result|
+    size = 0
+    result.each do
+      size += 1
+    end
+    size
+  end
+end
+
+#For adding data to the database using a http request
+post "/:id" do |env|
+  id = env.params.url["id"]
+  db.exec("INSERT INTO times (id) VALUES (?)", id.to_s)
+  "Added #{id} to database"
+end
+
+Kemal.run
+
+ensure
+  db.close
+  puts "Closing database"
+  Kemal.stop
+  puts "Stopping Server"
+end
